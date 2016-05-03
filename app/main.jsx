c795bcd63e44e34799e3ee9f1ff74ac95b656051
Ting Deng <ytdeng1@gmail.com>
2016-05-02 21:13:28 -0700

    set up gulp file to transpile react

---
+++ app/main.jsx
@@ -0,0 +1,107 @@
+// Required node modules
+var React = require('react');
+var ReactDOM = require('react-dom')
+// immedia React component dependencies
+// var ResultsView = require('./results.jsx');
+
+
+var MainView = React.createClass({
+
+  getInitialState: function(){
+    return {
+      searchTerm : 'immediahomepage',
+    };
+  },
+
+  componentWillMount: function(){
+    this.checkForSearchHistory();
+  },
+
+  render: function(){
+    return (
+      <div>
+        Hello from main.jsx
+      </div>
+    );
+  },
+
+  // Checks localStorage for history, which is stored in an array
+  // with element 0 being the most recent page. The number 0 
+  // corresponds with the home-page.
+  checkForSearchHistory: function() {
+    if (localStorage['immedia']) { 
+      var history = JSON.parse(localStorage['immedia']);
+    }
+    var homePlaceholder = { searchTerm: 0 };
+    if (!history) {
+      //to-do: get most popular searches
+      localStorage['immedia'] = JSON.stringify([homePlaceholder]);
+    } else if (history[0].searchTerm !== 0) { 
+      this.searchInit(history[0].searchTerm, true);
+    }
+  },
+
+  // This function is passed down through 'props' to both the search-bar
+  // on the home-page and the search-bar on the results-page
+  searchInit: function(searchTerm, reload){
+    // The parameter 'reload' tells us whether the function was called on a page-reload or
+    // from a geniune search. If genuine, we want to add it to our history:
+    if (!reload) {
+      this.enqueue(searchTerm);
+    }
+    // Setting 'atHome' to false routes the home-page to the results-page
+    this.setState({ 
+      atHome : false,
+      searchTerm: searchTerm,
+    });
+  },
+
+  //Enqueues new search to user history (stored in localStorage).
+  enqueue: function(searchTerm){
+    // Removes page from history if it's being searched now (so there is no redundancy when we
+    // add it to the front)
+    var history = JSON.parse(localStorage['immedia']),
+        newHist = [];
+
+    for (var i = 0; i < history.length; i++) {
+      if (searchTerm !== history[i].searchTerm) {
+        newHist.push(history[i]);
+      }
+    }
+    history = newHist;
+
+    // This if/else statement ensures that no placeholders for the home-page, inputted above,
+    // get kept in our history. We only want to know if the most recent page was the home-page,
+    // for page-reload purposes. Otherwise, we don't keep track of it in the search-history
+    // stored in localStorage.
+    var store = { searchTerm: searchTerm };
+    if (history[0].searchTerm === 0) {
+      history[0] = store;
+    } else {
+      var length = history.unshift(store);
+    }
+   
+    // Only the latest 10 pages are stored.
+    if (length > 10) { 
+      history.pop();
+    }
+    localStorage['immedia'] = JSON.stringify(history);
+  },
+
+  // Does the reverse of searchInit (routes to home-page by hiding results-page components).
+  goBackHome: function(){
+    this.setState({atHome : true });
+
+    // This block stores the home-page as the last visited location, so that if the user reloads the page
+    // when they are at the home-page, they get redirected to the home-page 
+    var history = JSON.parse(localStorage['immedia']);
+    // 'If' statement keeps the home-page from being added multiple times to the user's history
+    if (history[0].searchTerm !== 0) {
+      // Again, a value of 0 for 'searchTerm' indicates being at the home-page
+      history.unshift({ searchTerm: 0 });
+    }
+    localStorage['immedia'] = JSON.stringify(history);
+  },
+});
+
+ReactDOM.render(<MainView />, document.getElementById('app-root'));
---
+++ gulpfile.js
@@ -0,0 +1,184 @@
+var gulp        = require('gulp');
+var insert      = require('gulp-insert');
+var sass        = require('gulp-sass');
+var runSequence = require('run-sequence');
+var $           = require('gulp-load-plugins')();
+var babel       = require('gulp-babel');
+
+var minifyHTML  = require('gulp-minify-html');
+var minifyInline= require('gulp-minify-inline');
+
+var browserify  = require('browserify');
+var reactify    = require('reactify');
+var source      = require('vinyl-source-stream');
+var streamify   = require('gulp-streamify');
+var watchify    = require('watchify');
+var nodemon     = require('gulp-nodemon');
+
+
+
+//More seemless Gulp Task Management
+var path = {
+  HTML: './app/index.html',
+  MINIFIED_OUT: 'build.min.js',
+  OUT: 'build.js',
+  DEST: 'dist',
+  DEST_BUILD: 'dist/build',
+  DEST_SRC: 'dist/src',
+  ENTRY_POINT: './app/main.jsx'
+};
+
+//Default Tasks
+gulp.task('sass', function () {
+    gulp.src('app/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        
+    gulp.src('app/css/skins/default/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest("app/css/skins/default"))
+        
+    gulp.src('app/pages/auth/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest("app/pages/auth"))
+});
+
+gulp.task('insert', function(){
+    gulp.src(['app/css/skins/default/color-vars.css'])
+        .pipe(insert.append('</style>'))
+        .pipe(insert.prepend('<style is=\'custom-style\'>'))
+        .pipe(gulp.dest('app/css/skins/default/'));
+});
+
+gulp.task('sass-copy', function(){
+
+    var colorVars = gulp.src(['app/css/skins/default/color-vars-body.css'])
+        .pipe($.rename('color-vars.css'))
+        .pipe(gulp.dest('app/css/skins/default'));
+
+                
+    gulp.src(['app/components/pd-dashboard/pd-dashboard.html'])
+        .pipe($.rename('pd-dashboard.vulcanized.html'))
+        .pipe(gulp.dest('app/components/pd-dashboard'));
+        
+    return colorVars;
+});
+
+gulp.task('vulcanize', function () {
+    return gulp.src('app/components/pd-dashboard/pd-dashboard.vulcanized.html')
+        .pipe($.vulcanize({
+            stripComments: true,
+            inlineCss: false,
+            inlineScripts: true,
+            excludes: [
+                'bower_components/polymer/polymer.html'
+            ],
+            stripExcludes: []
+        }))
+        .pipe(gulp.dest('app/components/pd-dashboard'));
+});
+
+gulp.task('minify-html', function() {
+    var opts = {
+        conditionals: true,
+        spare:true,
+        quotes: true,
+        empty: true
+    };
+
+    return gulp.src('app/components/pd-dashboard/pd-dashboard.vulcanized.html')
+        .pipe(minifyHTML(opts))
+        .pipe(gulp.dest('app/components/pd-dashboard'));
+});
+
+gulp.task('minify-inline', function() {
+    gulp.src('app/components/pd-dashboard/pd-dashboard.vulcanized.html')
+        .pipe(minifyInline())
+        .pipe(gulp.dest('app/components/pd-dashboard'))
+});
+
+
+
+//*MINE*//
+
+
+gulp.task('copy', function(){
+  gulp.src(path.HTML)
+    .pipe(gulp.dest(path.DEST));
+});
+
+//Concatenates and Minifies our JS files
+//concates all JSX files, renames it then saves it in the dist folder
+
+gulp.task('build', function(){
+  browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify]
+  })
+  .bundle()
+  .pipe(source(path.MINIFIED_OUT))
+  .pipe(gulp.dest(path.DEST_BUILD))
+  console.log('One moment creating minified source build');
+
+  browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify]
+  })
+  .bundle()
+  .pipe(source(path.OUT))
+  .pipe(gulp.dest(path.DEST_SRC))
+  console.log('One moment creating source build');
+
+});
+
+//Watches for any changes in our JSX files to javascript
+gulp.task('watch', function(){
+  gulp.watch(path.HTML, ['copy'])
+
+  var watcher = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function(){
+    watcher.bundle()
+      .pipe(source(path.MINIFIED_OUT))
+      .pipe(gulp.dest(path.DEST_BUILD))
+      console.log('Updated Minified Source Build');
+  })
+    .bundle()
+    .pipe(source(path.MINIFIED_OUT))
+    .pipe(gulp.dest(path.DEST_BUILD))
+})
+
+//restarts the server whenever there is any change on server/server.js
+gulp.task('nodemon', function(){
+  console.log('in nodemon')
+  nodemon({
+    script: './demo-server/app.js',
+    ext: 'js',
+    // tasks: [],
+    env: {'NODE_ENV': 'development'}
+  })
+  .on('start', function(){
+    console.log('Server Started!!!');
+  })
+  .on('restart', function(){
+    console.log('Server Restarted!!!');
+  })
+});
+
+
+gulp.task('default',  function (cb) {
+    runSequence(
+        ['sass', 'sass-copy'],
+        ['insert',
+        'vulcanize'],
+        'minify-html',
+        'minify-inline',
+        cb)
+});
+
+gulp.task('run', ['copy', 'watch', 'nodemon']);
